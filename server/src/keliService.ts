4cec7714d7ea45f9ddd765e595b0029f9a92b5ad
Wong Jia Hau <hou32hou@gmail.com>
2019-03-06 16:30:07 +0800

    chore: updated KELI_COMPILER_PATH

--- server/src/keliService.ts
+++ server/src/keliService.ts
@@ -1,7 +1,7 @@
 import { Diagnostic, CompletionItem, Position } from 'vscode-languageserver';
 
 export class KeliService {
-	private static KELI_COMPILER_PATH = "/home/hou32hou/Repos/keli/compiler/.stack-work/install/x86_64-linux/lts-13.0/8.6.3/bin/keli-compiler-exe";
+	private static KELI_COMPILER_PATH = "keli";
 	public static analyze(fileContents: string): Promise<Diagnostic[]> {
 		return this.runCommand(fileContents, "--analyze", [])
 			.then((result) => JSON.parse(result).map((x) => ({...x, message: x.message.trim(), source: "[keli]"})));
